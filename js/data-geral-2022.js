8b69abb4673f77dc043c72b14ab97fda47a10974
medeirosld <medeirosld28@gmail.com>
2026-08-01 20:51:04 -0400

    correção toltips

--- js/data-geral-2022.js
+++ js/data-geral-2022.js
@@ -459,6 +459,7 @@ function shouldUseGeneralCityOfficialTotals(cargo = currentCargo) {
   const year = String(STATE.currentElectionYear);
   return (year === '2022' || year === '2018' || year === '2014' || year === '2010' || year === '2006' || year === '2002' || year === '1998' || year === '1994')
     && STATE.currentElectionType === 'geral'
+    && STATE.isFilterAggregationActive
     && currentCidadeFilter !== 'all'
     && currentBairroFilter === 'all'
     && !currentLocalFilter
