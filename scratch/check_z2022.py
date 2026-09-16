584b81994e7bb0359216ef14c2d1d43f07b79e8f
Franzisquin <fandangogamer821@gmail.com>
2026-09-16 16:40:04 -0300

    Limpeza e segurança

--- scratch/check_z2022.py
+++
@@ -1,3 +0,0 @@
-import zipfile
-with zipfile.ZipFile('resultados_geo/locais_votacao_2022_gpkg.zip') as z:
-    print(z.namelist())
--- scratch/inspect_gpkg_cities.py
+++
@@ -1,5 +0,0 @@
-import json
-with open('resultados_geo/municipios_hd/municipios_AL.geojson', encoding='utf-8') as f:
-    poly_geo = json.load(f)
-print("First feature properties in municipios_AL.geojson:")
-print(poly_geo['features'][0]['properties'])
